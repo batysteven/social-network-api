3490b12dad1dbe4014adec5d0164857326111405
Steven Baty <baty_steven@yahoo.com>
2021-04-16 18:54:43 -0600

    created router('/') .get(getAllUsers) .post(createUser), router('/:id') .get(getUserById) .put(updateUser) .delete(deleteUser), router(':userId/friends/friendId') .post(addFriend) .delete(deleteFriend)

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,2 +1,28 @@
 const router = require('express').Router();
+const {
+    getAllUsers,
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('')
 
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+    
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router
+    .route(':userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
+
+module.exports = router;
