047973da6b41d32259d5658e1cc43e928ae91e0e
Steven Baty <baty_steven@yahoo.com>
2021-04-16 18:29:12 -0600

    created ReationSchema reactionID/reactionBody/username/createdAt

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,34 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-
+const ReactionSchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent thought
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+)
 
 const ThoughtSchema = new Schema(
     {
@@ -32,10 +59,8 @@ const ThoughtSchema = new Schema(
 );
 
 // get total count of reactions
-ThoughtSchema.virtual('reactionCount').get(function() {
-    return this.reactions.reduce(
-        (total, reaction) => total + reaction.length + 1, 0
-    );
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
 });
 
 const Thought = model('Thought', ThoughtSchema);
